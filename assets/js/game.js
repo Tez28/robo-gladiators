fd88541ab6512fb2fdc201740e4feceec6661552
Tez28 <courtezcannady@gmail.com>
2023-02-22 19:07:21 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,14 +5,20 @@ var playerMoney = 10;
 // you can also log multiple values at once like this 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+// console.log(enemyNames.length);
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + "is at " + i + " index");
+// }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // console log enemy
-console.log (enemyName, enemyAttack, enemyHealth);
+//console.log (enemyName, enemyAttack, enemyHealth);
 // fight function
-var fight = function () {
+var fight = function (enemyName) {
     // alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -27,7 +33,7 @@ var fight = function () {
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
         // check enemy health
-        if (enemyHealth <=0) {
+        if (enemyHealth <= 0) {
             window.alert(enemyName + " has died!");
         } else {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
@@ -69,4 +75,8 @@ var fight = function () {
 
 
 // execute function
-fight();
+//fight();
+// fight function executed in for loop 
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
